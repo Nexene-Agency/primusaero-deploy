5e58922a3a45c77c8798a4fbe60ca3e0e0a0c78d
Viktor Feurer <viktor.feurer@defz.io>
2023-11-24 17:39:38 +0000

    Merged PR 33: AB#68 maintenance subpage implemented
    AB#68 maintenance subpage implemented
    
    Related work items: #68

---
+++ app/[locale]/(customLayout)/services/maintenance/messages.ts
@@ -0,0 +1,24 @@
+const LOCAL_MESSAGES = {
+  "en": {
+    titles: {
+      1: "Maintenance",
+    },
+    sections: {
+      section0: {
+        title: "Choice of maintenance facility",
+        text: "We assist our clients with finding a qualified maintenance facility close to their aircraft's location.",
+      },
+      section1: {
+        title: "Invoice auditing",
+        text: "Our services offer thorough financial auditing, where we carefully review supplier invoices to ensure pricing accuracy and correctness. Our extensive expertise allows us to assess invoiced costs and negotiate discounted prices.<br/>This process results in significant savings on fees for our clients.",
+      },
+      section2: {
+        title: "Operational auditing",
+        text: "Our operational auditing helps our clients stay compliant and identify gaps before regulators do.",
+      },
+    },
+    getInTouch: "Get in touch",
+  }
+};
+
+export default LOCAL_MESSAGES;
