02deb0abac0b5d57e6e80dafcd4d0a55d399739c
Viktor Feurer <viktor.feurer@defz.io>
2023-11-24 17:31:54 +0000

    Merged PR 32: AB#65 aircraft charter subpage implemented
    AB#65 aircraft charter subpage implemented
    
    Related work items: #65

---
+++ app/[locale]/(customLayout)/services/aircraft-charter/messages.ts
@@ -0,0 +1,16 @@
+const LOCAL_MESSAGES = {
+  "en": {
+    titles: {
+      1: "Aircraft Charter",
+    },
+    sections: {
+      section0: {
+        title: "",
+        text: "",
+      },
+    },
+    getInTouch: "Get in touch",
+  }
+};
+
+export default LOCAL_MESSAGES;
